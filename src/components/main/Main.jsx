e6ae84f13fcc466e604f9722ed570d6e3f935b1d
Lapina Maria <animashka.333@gmail.com>
2023-02-12 21:37:55 +0300

    main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,29 @@
 
 import css from './Main.module.css'
+import MainImg1 from '../../assets/main/main-1.svg'
+import MainImg2 from '../../assets/main/main-2.svg'
 
 const Main = props => {
 	return (
 		<main className={css.main} >
-
+			<section className={css.infoBlock}>
+				<div>
+					<h1 className={css.title}>сервис по поиску публикаций о компании по его ИНН</h1>
+					<p className={css.info}>Комплексный анализ публикаций, получение данных в формате PDF на электронную почту.</p>
+					<button className='button'>Запросить данные</button>
+				</div>
+				<div>
+					<img src={MainImg1} alt='человек'/>
+				</div>
+			</section>
+			<section className={css.carousel}>
+				<h2 className={css.titleH2}>Почему именно мы</h2>
+				
+				<img className={css.imgMan} src={MainImg2} alt='человек с галочкой'></img>
+			</section>
+			<section className={css.tariff}>
+				<h2 className={css.titleH2}>наши тарифы</h2>
+			</section>
 		</main>
 	)
 }
